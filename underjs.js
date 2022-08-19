869ba6280046a0c2376bfc6fc402d7af43543b2b
ameer hamza <ameersoftdev@gmail.com>
2022-08-19 12:51:30 +0500

    var let const

--- underjs.js
+++ underjs.js
@@ -1,1 +1,16 @@
-alert("ok the report");
+// function checkScope() {  // var shadowing
+//   var a = "hi i m var";
+//   let b = "hi i am let";
+//   if (true) {
+//     const a = "i am const shadowing";
+//     var b = "i am var shadowing";
+//     console.log(a);
+//     console.log(b);
+//   }
+// }
+
+// checkScope();
+
+/// important question /////
+// hoisting
+// while creation phase javascript move variabales and functions on top of the code this phase is called hoisting
